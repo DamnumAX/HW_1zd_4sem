654f25074660e51c6be6a920baf868ff82aa595e
Filipp G <filfilet@mail.ru>
2022-12-08 17:59:17 +0300

    first commit

---
+++ dz_1zd_4sem/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Напишите программу, которая принимает на вход два числа (A и B) и метод который возводит число A в натуральную степень B. 
+Задание должно быть выполнено в методе. В методе не должно быть вывода в консоль. 
+Не использовать Math.Pow()
+*/
+
+
+bool isParsed = int.TryParse(Console.ReadLine(), out int number);
+
+if(!isParsed || number < 1)
+{
+    Console.WriteLine("Некорректное число");
+    return;
+}
+int stepen = GetStepenA(number);
+
+Console.WriteLine(stepen);
+
+int GetStepenA(int number)
+{
+    int a = number;
+    int b = Convert.ToInt32(Console.ReadLine());
+    int stepen = a;
+   for (int i = 1; i < b; i++)
+    {
+        stepen = stepen * a;
+    }
+    return stepen;
+}
